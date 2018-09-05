1e9552ccdd4ed6ec81ac61800099bc389caa7a0d
cglaitong <cglaitong@unal.edu.co>
2018-09-05 20:42:23 +0000

    primer commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,59 @@
 class BooksController < ApplicationController
-    def index 
-        books = Book.all
-        respond_to do |format|
-            format.json {render json: books , status:200 }
+
+  #se utiliza para ver los datos
+  #respond_to es una funcion que viene de ApplicationController
+    def index
+        author= Author.find(params[:author_id])
+        if author 
+        books = author.books
+            render jason: books, status: 200
         end
-       
     end
-    
-    def create
-        book = Book.new(params_book)
-        if book.save 
-            respond_to do |format|
-            format.html {render html: books , status:200 }
-            format.json {render json: books , status:200 }
-        end
-        end 
-    else 
-        respond_to do |format|
-            format.json {render json: books , status:404 }
-        end
-            
-        
-    end
+
+
     def show
         book = Book.find(params[:id])
         respond_to do |format|
-            format.jason {render jason: book, status:200}
+            format.json {render json: book, status:200}
+        end
+    end
+    #crea un nuevo registro para la base de datos
+    def create
+        book = Book.new(params_book)
+        if book.save
+            respond_to do |format|
+                format.json {render json: book, status:201}
+            end
+        else
+            respond_to do |format|
+                format.json {render json: book.errors, status: :unprocessable_entity}
+            end
+        end
+    end
+    
+    
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        respond_to do |format|
+                format.json {render json: book, status:201}
+            end
+        
+        
+    end
+    
+    
+    def update
+          if book = Book.find(params[:id])
+            book.update(params_book)
+            render json: book, status:200
+        else
+             render json: book.errors, status:422
+        end
+       
     end
     def params_book
         params.permit(:name, :year, :code, :avaliable)
     end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :books
+  resources :authors do
+    resources :books
+  end
+  
+  get 'books/listBooks',  to:  'books#listBooks'
  
 end
